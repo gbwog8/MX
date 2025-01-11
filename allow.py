e455292a02941c6de2ee7fec3ea5dd1a2d6a57ee
OnlineMo <143324986+OnlineMo@users.noreply.github.com>
2025-01-11 19:39:30 +0800

    Update allow.py

--- allow.py
+++ allow.py
@@ -19,34 +19,7 @@ async def main():
     page = await browser.newPage()
 
     # 打开目标网页
-    await page.goto(renew_url)
-
-    # 获取复选框元素并点击
-    checkbox = await page.querySelector('input[type="checkbox"]')
-    if checkbox:
-        await checkbox.click()
-    else:
-        print('复选框未找到')
-
-    # 获取输入框元素并设置值
-    input_field = await page.querySelector('.form-control.is-invalid')
-    if input_field:
-        await input_field.type(登录密码)
-    else:
-        print('输入框未找到')
-
-    # 获取提交按钮元素并点击
-    submit_button = await page.querySelector('button[type="submit"]')
-    if submit_button:
-        await submit_button.click()
-    else:
-        print('提交按钮未找到')
-
-    # 访问链接
-    await page.goto(f'{renew_url}/System/SpecialPardon')
-
-    # 关闭浏览器
-    await browser.close()
-
-# 运行主函数
-asyncio.run(main())
+    try:
+        await page.goto(renew_url, {'timeout': 60000})  # 增加超时时间到 60 秒
+    except Exception as e:
+        print
